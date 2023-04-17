185e8225cd7aa535e7e675b58147c7ccf6546200
Yongha <loha31707@gmail.com>
2023-04-18 01:02:13 +0900

    Init

---
+++ YHDictionary/Data_Structure/YHDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure
+{
+    class YHDictionary<TKey, TValue>
+    {
+        struct Bucket
+        {
+            TKey key;
+            TValue value;
+            int hashcode;
+            int next;
+        }
+
+        Bucket[] buckets;
+
+
+
+
+        public YHDictionary() : this(0) { }
+        public YHDictionary(int capacity)
+        {
+            if (capacity < 0) throw new ArgumentOutOfRangeException("Capacity 제대로 넣어라잉");
+        }
+    }
+}
